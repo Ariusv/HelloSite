c3cb27142bebf377430e45e562141792ee9324cb
Vitalii Nastosiak <75575669+Ariusv@users.noreply.github.com>
2022-01-21 19:56:48 +0200

    Update settings.py

--- hellohuman/settings.py
+++ hellohuman/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-django_heroku.setings(locals())
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
