c4b5a4d6bac7f40e5ef0ed3c0f87ec3566a055fe
Vitalii Nastosiak <75575669+Ariusv@users.noreply.github.com>
2022-01-21 19:53:50 +0200

    Update settings.py

--- hellohuman/settings.py
+++ hellohuman/settings.py
@@ -1,6 +1,9 @@
 import os
 from pathlib import Path
 
+import django_heroku
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -13,14 +16,13 @@ SECRET_KEY = 'django-insecure-!l_j$#y3$1k0x2z28o%71xbr5(7#q8dl)=_z2j$!(bxj2t=j^x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ['hellohumans.herokuapp.com']
 
 # Application definition
 
 INSTALLED_APPS = [
     'main.apps.MainConfig',
     'crispy_forms',
-    'debug_toolbar',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,7 +41,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'hellohuman.urls'
@@ -67,11 +68,12 @@ WSGI_APPLICATION = 'hellohuman.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hellohumansdb',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': "localhost",
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd4miaffdqovtu4',
+        'USER': 'qksdqslchlfijc',
+        'PASSWORD': '3d5bfebfaf103cb38f9702bce3c59f7e1c82682cecdfeb88a184e74c8df8d511',
+        'HOST': "ec2-176-34-105-15.eu-west-1.compute.amazonaws.com",
+        'PORT': '5432',
 
     }
 }
@@ -108,20 +110,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+django_heroku.setings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
 
 CACHES = {
     'default': {
